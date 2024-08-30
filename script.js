81694ef9e394cf80a2f56563c8e43cc2071d1c50
ViolinistaEstudante <132099734+ViolinistaEstudante@users.noreply.github.com>
2024-08-29 21:29:06 -0300

    Site atualizado!!

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 const professorQuotes = [
-    { quote: "A motivação te leva a começar. A determinação te faz continuar", author: "Profª Gabriela - Português", imageUrl: "https://github.com/ViolinistaEstudante/chamado-escolar/assets/132099734/4893694c-68ad-4b24-b941-615997e26c0f"},
-    { quote: "Vencer é uma mistura de luta, esforço, otimismo e não desistir nunca", author: "Prof° Élio - Biologia", imageUrl: "https://github.com/ViolinistaEstudante/chamado-escolar/assets/132099734/3a4ce975-9197-4cab-9788-12148020da09"},
-    { quote: "Estude, pois a caneta é mais leve do que a pá", author: "Profª Karol - Física", imageUrl:"https://github.com/ViolinistaEstudante/chamado-escolar/assets/132099734/5e994848-48ed-47f6-bf27-b62f0f04dce5"},
-    { quote: "O que você está fazendo por você?", author: "Prof° Sidnei - Filosofia", imageUrl:"https://github.com/ViolinistaEstudante/chamado-escolar/assets/132099734/93510724-6223-4ecc-a774-263960d70f78"},
-    { quote: "Superação é quando te amassa a alma, e você ainda briga.", author: "Profª Danielli - Ed. Física", imageUrl: "https://github.com/ViolinistaEstudante/chamado-escolar/assets/132099734/7c9e334f-c48d-46bf-972d-e14bbd1d516a"},
-    { quote: "Precisamos de vocês alunos, para o nosso futuro", author: "Profª Maria Marli - Inglês", imageUrl: "https://github.com/ViolinistaEstudante/chamado-escolar/assets/132099734/47fffd72-03bb-48c9-8a69-99b8b5ca737a"},
-    { quote: "União, dedicação, são fundamentais para seu futuro.", author: "Profº Luiz Paulo - Geografia", imageUrl: "https://github.com/ViolinistaEstudante/chamado-escolar/assets/132099734/a2a4d8ce-66ef-4c2f-84e0-f1f917c98597"},
-    { quote: "A educação pode ser a única ponte entre você e o sucesso", author: "Profª Érica - Química", imageUrl: "https://github.com/ViolinistaEstudante/chamado-escolar/assets/132099734/746a148b-3694-48cf-90d3-b5cff8369e49"},
-    { quote: "Apenas a educação tem o poder de livrar!", author: "Prof° Victor - Matemática", imageUrl: "https://github.com/ViolinistaEstudante/chamado-escolar/assets/132099734/9141c189-f7cb-463f-b350-6a0a2ba43437"},
-    { quote: "Cada um tem a visão da montanha que subiu", author: "Profª Karine - História", imageUrl: "https://github.com/user-attachments/assets/09ad74f4-b902-4cc9-9ea1-88844241567a"}
+    { quote: "A motivação te leva a começar. A determinação te faz continuar", author: "Profª Gabriela - Português", imageUrl: "https://github.com/user-attachments/assets/7beadd45-a8c0-4a32-b08a-b5fb68e8a9d5"},
+    { quote: "Vencer é uma mistura de luta, esforço, otimismo e não desistir nunca", author: "Prof° Élio - Biologia", imageUrl: "https://github.com/user-attachments/assets/4e06e268-e806-446c-a7c7-20b6d77debc9"},
+    { quote: "Estude, pois a caneta é mais leve do que a pá", author: "Profª Karol - Física", imageUrl:"https://github.com/user-attachments/assets/6e02acc5-539b-40cc-acca-9cbf3ab51c0e"},
+    { quote: "O que você está fazendo por você?", author: "Prof° Sidnei - Filosofia", imageUrl:"https://github.com/user-attachments/assets/bd123016-890b-4fc6-8aac-6bee695fe979"},
+    { quote: "Superação é quando te amassa a alma, e você ainda briga.", author: "Profª Danielli - Ed. Física", imageUrl: "https://github.com/user-attachments/assets/f0ab9563-62d6-4e06-adf8-3fde80361d7b"},
+    { quote: "Precisamos de vocês alunos, para o nosso futuro", author: "Profª Maria Marli - Inglês", imageUrl: "https://github.com/user-attachments/assets/af2ef0e4-e59a-4454-84d7-e45c7892661c"},
+    { quote: "União, dedicação, são fundamentais para seu futuro.", author: "Profº Luiz Paulo - Geografia", imageUrl: "https://github.com/user-attachments/assets/180fd87c-3a2d-47cf-8a54-3b418d51e358"},
+    { quote: "A educação pode ser a única ponte entre você e o sucesso", author: "Profª Érica - Química", imageUrl: "https://github.com/user-attachments/assets/d1411788-65c6-441d-ba79-e6ba5c9f6759"},
+    { quote: "Apenas a educação tem o poder de livrar!", author: "Prof° Victor - Matemática", imageUrl: "https://github.com/user-attachments/assets/acf3075d-b8da-46d1-9493-9a61a567af25"},
+    { quote: "Cada um tem a visão da montanha que subiu", author: "Profª Karine - História", imageUrl: "https://github.com/user-attachments/assets/b52ed346-3b8a-439e-8408-9fdf48bab63c"}
 ];
 
 const studentQuotes = [
-    { quote: "Seu futuro começa com um simples passo em direção ao conhecimento.", author: "Estudantes - 2°C", imageUrl: "https://github.com/ViolinistaEstudante/chamado-escolar/assets/132099734/4bff56c8-a8fa-42c9-86f8-e7c4576cb00d" },
-    { quote: "Seja curioso, seja criativo, seja bem-sucedido. Tudo isso começa com o ato de estudar", author:"Estudantes - 2°C", imageUrl: "https://github.com/ViolinistaEstudante/chamado-escolar/assets/132099734/3edfb72d-daa1-4b94-ae75-ed5fdb83d20f" },
+    { quote: "Seu futuro começa com um simples passo em direção ao conhecimento.", author: "Estudantes - 2°C", imageUrl: "https://github.com/user-attachments/assets/f4eb6355-86cd-4569-8e38-c3d60f2861c1" },
+    { quote: "Seja curioso, seja criativo, seja bem-sucedido. Tudo isso começa com o ato de estudar", author:"Estudantes - 2°C", imageUrl: "https://github.com/user-attachments/assets/e4e76ff1-d3ca-4167-83c4-1b2c31708fb4" },
 ];
 
 const externalAuthorQuotes = [
-    { quote: "Não deixe que o medo de errar o impeça de tentar.", author: "- Brian Tracy", imageUrl: "https://github.com/ViolinistaEstudante/chamado-escolar/assets/132099734/62205433-0824-4475-a326-1ff6aecd51af" },
-    { quote: "A chave do sucesso é a persistência.", author: "- Napoleon Hill", imageUrl: "https://github.com/ViolinistaEstudante/chamado-escolar/assets/132099734/fb331c24-cbf3-4475-9eac-a7cae9de98db" },
-    { quote: "Você é capaz de muito mais do que imagina.", author: "- Zig Ziglar", imageUrl: "https://github.com/ViolinistaEstudante/chamado-escolar/assets/132099734/664e79a9-e6eb-4f13-8c80-fdcc59ba6c26" },
-    { quote: "Sempre há um caminho quando há vontade.", author: "- Pablo Neruda", imageUrl: "https://github.com/ViolinistaEstudante/chamado-escolar/assets/132099734/f4bbbbab-4c40-4491-a51b-be48db234a5f" },
-    { quote: "A única limitação que você tem é a que você impõe a si mesmo.", author: "- Les Brown", imageUrl: "https://github.com/ViolinistaEstudante/chamado-escolar/assets/132099734/a5c0285b-1234-463b-9f35-5ee278a74842" },
-    { quote: "Não deixe que o passado determine o seu futuro.", author: "- Eckhart Tolle", imageUrl: "https://github.com/ViolinistaEstudante/chamado-escolar/assets/132099734/93a62ecd-6e54-4624-a4ee-a0fee75ca746" },
-    { quote: "O sucesso é a soma de pequenos esforços repetidos dia após dia.", author: "- Robert Collier", imageUrl: "https://github.com/ViolinistaEstudante/chamado-escolar/assets/132099734/a0a175a4-78b7-4716-bbe5-532925a6103f" },
+    { quote: "Não deixe que o medo de errar o impeça de tentar.", author: "- Brian Tracy", imageUrl: "" },
+    { quote: "A chave do sucesso é a persistência.", author: "- Napoleon Hill", imageUrl: "" },
+    { quote: "Você é capaz de muito mais do que imagina.", author: "- Zig Ziglar", imageUrl: "" },
+    { quote: "Sempre há um caminho quando há vontade.", author: "- Pablo Neruda", imageUrl: "" },
+    { quote: "A única limitação que você tem é a que você impõe a si mesmo.", author: "- Les Brown", imageUrl: "" },
+    { quote: "Não deixe que o passado determine o seu futuro.", author: "- Eckhart Tolle", imageUrl: "" },
+    { quote: "O sucesso é a soma de pequenos esforços repetidos dia após dia.", author: "- Robert Collier", imageUrl: "" },
 ];
 
 // Função para exibir uma nova frase
